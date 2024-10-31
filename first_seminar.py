7bea30f04277bfb04e068480a1e3e9eb0489f631
monterrubio12 <alvaro.monterrubio01@estudiant.upf.edu>
2024-10-31 18:20:15 +0100

    Cambios RGB YUV

--- first_seminar.py
+++ first_seminar.py
@@ -1,11 +1,13 @@
 class exercises:
-    def RGBtoYUV(r,g,b):
-        Y= 0.257*R + 0.504*G + 0.098*B + 16
-        U= -0.148*R - 0.291*G +0.439*B + 128
-        V= 0.439*R - 0.368*G - 0.071*B + 128
-        return Y,U,V
+   def RGBtoYUV(r,g,b):
+       Y= 0.257*R + 0.504*G + 0.098*B + 16
+       U= -0.148*R - 0.291*G +0.439*B + 128
+       V= 0.439*R - 0.368*G - 0.071*B + 128
+       return Y,U,V
 
-    def YUVtoRGB(Y,U,V)
-        R= 1.164*(Y-16)
 
-    #CAMBIOS ALEXX
+   def YUVtoRGB(Y,U,V)
+       R= 1.164*(Y-16) + 2.018*(U-128)
+       G= 1.164*(Y-16) - 0.813*(V-128) - 0.391*(U-128)
+       B= 1.164*(Y-16) + 1.596*(V-128)
+       return R,G,B
