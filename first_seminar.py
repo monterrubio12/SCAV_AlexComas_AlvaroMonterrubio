ac5f2163e3d7b290574d0950c93ba7dee10b81b3
monterrubio12 <alvaro.monterrubio01@estudiant.upf.edu>
2024-10-31 17:43:24 +0100

    RGBtoYUV implementation 1.1

--- first_seminar.py
+++ first_seminar.py
@@ -1,5 +1,9 @@
 class exercises:
     def RGBtoYUV(r,g,b):
-        Y=
-        U=
-        V=
+        Y= 0.257*R + 0.504*G + 0.098*B + 16
+        U= -0.148*R - 0.291*G +0.439*B + 128
+        V= 0.439*R - 0.368*G - 0.071*B + 128
+        return Y,U,V
+
+    def YUVtoRGB(Y,U,V)
+        R= 1.164*(Y-16)
