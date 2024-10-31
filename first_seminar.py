7bb1cb43c2c3603bacd387dd2234778bc0629989
Comas20 <alex.comas01@estudiant.upf.edu>
2024-10-31 18:04:11 +0100

    cambio alex

--- first_seminar.py
+++ first_seminar.py
@@ -1,11 +1,11 @@
 class exercises:
     def RGBtoYUV(r,g,b):
-        Y= 0.257*g + 0.504*g + 0.098*b + 16
-        U= -0.148*g - 0.291*g +0.439*b + 128
-        V= 0.439*g - 0.368*g - 0.071*b + 128
+        Y= 0.257*R + 0.504*G + 0.098*B + 16
+        U= -0.148*R - 0.291*G +0.439*B + 128
+        V= 0.439*R - 0.368*G - 0.071*B + 128
         return Y,U,V
 
     def YUVtoRGB(Y,U,V)
         R= 1.164*(Y-16)
 
-    #cambios
+    #VERSION ALEX
