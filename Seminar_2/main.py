5d770f945c746237dd69bc2d99a789b7c90de986
monterrubio12 <alvaro.monterrubio01@estudiant.upf.edu>
2024-11-28 18:14:33 +0100

    CAMBIOS ALVARO SEMI 2

---
+++ Seminar_2/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from test_main import app as test_app
+
+app = FastAPI()
+app.mount("/test", test_app)
+
+@app.get("/")
+async def root():
+    return {"message": "Practice 1 by Alex Comas & Alvaro Monterrubio"}
+
+
+#HACER TEST CON ENDPOINTS:
+
+#Test RGB to YUV: /test/test_rgb_to_yuv/
+##Test YUV to RGB: /test/test_yuv_to_rgb/
+#Test Serpentine: /test/test_serpentine/
+#Test Resize: /test/test_resize/
+#Test Black & White Conversion: /test/test_bw_converter/
+#Test Run-Length Encoding: /test/test_run_length_encoding/
+#Test DCT Encoding: /test/test_dct_encoding/
+#Test DWT Encoding: /test/test_dwt_encoding/
