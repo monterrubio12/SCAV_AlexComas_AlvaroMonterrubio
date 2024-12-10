73fbe6a13e29d7be1b75ab11d651806547275a1a
monterrubio12 <alvaro.monterrubio01@estudiant.upf.edu>
2024-12-10 15:56:28 +0100

    API Funcionando Álvaro

--- Practice_2/practice_2.py
+++ Practice_2/practice_2.py
@@ -13,73 +13,50 @@ import pywt
 
 class transcoding_utils_comas_alvaro: 
     def convert_to_multiple_formats(self, input_file, type):
- 
-        if (type=="H265"):
-        # Convertimos el video a H265
+        output_dir = os.path.dirname(input_file)
+
+        # Tipos de codec con los que trabvajaremos y como se exportaran
+        output_files = {
+            "H265": os.path.join(output_dir, "output_h265.mp4"),
+            "VP9": os.path.join(output_dir, "output_vp9.webm"),
+            "AV1": os.path.join(output_dir, "output_av1.mkv"),
+            "VP8": os.path.join(output_dir, "output_vp8.webm"),
+        }
+
+        # En función de la petición, decidimos que proceso ejecutamos con condiciones if
+        if type == "H265":
             subprocess.run(
                 [
                     "ffmpeg", "-i", input_file, 
                     "-c:v", "libx265", "-crf", "26", "-preset", "fast", 
-                    "-c:a", "aac", "-b:a", "128k", 
-                    "output_h265.mp4"
+                    "-c:a", "aac", "-b:a", "128k", output_files["H265"]
                 ],
                 check=True
             )
-        
-
-        # Convertimos a VP9 en dos pasos, dentro de FFMPEG hemos visto que es lo recomendado para mantener algunos ajustes de calidad
-        #Primer paso (Pass1) -> Usa el códec libvpx-vp9 para analizar el archivo de entrada
-
-        if (type=="VP9"):
+        #Intentamos hacer el de dos pasos pero peta, lo realizamos solo con uno
+        elif type == "VP9":
             subprocess.run(
                 [
                     "ffmpeg", "-i", input_file, 
-                    "-c:v", "libvpx-vp9", "-b:v", "2M", "-pass", "1", 
-                    "-an", "-f", "null", "/dev/null"
+                    "-c:v", "libvpx-vp9", "-b:v", "2M", output_files["VP9"]
                 ],
                 check=True
             )
-            
-
-            # Segundo paso (Pass 2) -> Usa las estadísticas generadas en el primer paso para realizar la codificación optimizada
+        elif type == "AV1":
             subprocess.run(
-                [
-                    "ffmpeg", "-i", input_file, 
-                    "-c:v", "libvpx-vp9", "-b:v", "2M", "-pass", "2", 
-                    "-c:a", "libopus", "output_vp9.webm"
-                ],
+                ["ffmpeg", "-i", input_file, "-c:v", "libaom-av1", "-crf", "30", output_files["AV1"]],
                 check=True
             )
-        
-
-        if (type=="AV1"):
-        # AV1 Conversion
+        elif type == "VP8":
             subprocess.run(
-                [
-                    "ffmpeg", "-i", input_file, 
-                    "-c:v", "libaom-av1", "-crf", "30", 
-                    "output_av1.mkv"
-                ],
-                check=True
-            )
-        
-
-        # VP8 Conversion
-        if (type=="VP8"):
-            subprocess.run(
-                [
-                    "ffmpeg", "-i", input_file, 
-                    "-c:v", "libvpx", "-b:v", "1M", 
-                    "-c:a", "libvorbis", 
-                    "output_vp8.webm"
-                ],
+                ["ffmpeg", "-i", input_file, "-c:v", "libvpx", "-b:v", "1M", "-c:a", "libvorbis", output_files["VP8"]],
                 check=True
             )
         
     
     def encode_ladder(self, input_file, output_dir):
 
-
+        #Todas las resoluciones con las que trabajaremos y su sufijo
         resolutions = [
             ("1920x1080", "1080p"),
             ("1280x720", "720p"),
